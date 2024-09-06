b8cb5c51cd3fb69ba6f4751d1ba24c5a951dd7b9
Yathi Raj Bollimbala G <511386+yuth@users.noreply.github.com>
2024-09-06 14:02:19 -0700

    add isr page with /isr

---
+++ src/pages/isr.tsx
@@ -0,0 +1,16 @@
+export default function TestPage(props: { data: { t: string } }) {
+  return (
+    <div>
+      <h1>ISR Page</h1>
+      <pre>Page Rendered at {props.data.t}</pre>
+    </div>
+  );
+}
+
+// This gets called on every request
+export async function getStaticProps() {
+  // Fetch data from an API or other source
+  const data = { t: new Date().toISOString() };
+  // Revalidate this page every 60 seconds
+  return { props: { data: data }, revalidate: 60 };
+}
